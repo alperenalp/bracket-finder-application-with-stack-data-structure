846aac0324a4d100119072c2531df576ca09b142
alperenalp <alperen.alp045@gmail.com>
2022-03-19 13:55:52 +0300

    application

---
+++ my_bracket_finder_app/Odev4_Yigin_Yapisi/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿
+namespace Odev4_Yigin_Yapisi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.butonKontrol = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.tabPage1.Controls.Add(this.butonKontrol);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(574, 298);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Yığın Yapısı Ödev";
+            // 
+            // butonKontrol
+            // 
+            this.butonKontrol.Location = new System.Drawing.Point(223, 133);
+            this.butonKontrol.Name = "butonKontrol";
+            this.butonKontrol.Size = new System.Drawing.Size(116, 38);
+            this.butonKontrol.TabIndex = 5;
+            this.butonKontrol.Text = "KONTROL ET";
+            this.butonKontrol.UseVisualStyleBackColor = true;
+            this.butonKontrol.Click += new System.EventHandler(this.butonKontrol_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(157, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(237, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "İSTEDİĞİNİZ MATEMATİKSEL GİRİŞİ YAPINIZ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(160, 87);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(234, 40);
+            this.textBox1.TabIndex = 3;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(582, 324);
+            this.tabControl1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(582, 324);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Button butonKontrol;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TabControl tabControl1;
+    }
+}
+
---
+++ my_bracket_finder_app/Odev4_Yigin_Yapisi/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Odev4_Yigin_Yapisi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public class dugum
+        {
+            public char deger;
+            public dugum baglanti;
+        }
+        public dugum ilk;
+
+        private void butonKontrol_Click(object sender, EventArgs e)
+        {
+            string ifade = textBox1.Text;
+            int boyut = ifade.Length;
+            ilk = null;
+            Boolean dogruMu = true;
+            Boolean kontrol = true;
+            int acilan = 0;
+            int kapanan = 0;
+            for (int i = 0; i < boyut; i++)
+            {
+                char karakter = ifade[i];
+                if (karakter == '(' || karakter == '{' || karakter == '[')
+                {
+                    push(karakter);
+                    acilan++;
+                }
+
+                if (karakter == ')' || karakter == '}' || karakter == ']')
+                {
+                    if (isempty())
+                    {
+                        MessageBox.Show("Açma parantezinden eksik var.");
+                        dogruMu = false;
+                        break;
+                    }
+                    else
+                    {
+                        char top = peek(); // yığında tepedeki karakteri bulur
+                        if (top == '(' && karakter == ')' || top == '{' && karakter == '}' || top == '[' && karakter == ']')
+                        {
+                            pop();  // yığında tepedeki karakteri siler
+                            kapanan++;
+                        }
+                        else
+                        {
+                            MessageBox.Show("Parantezlerde uyuşmazlık var.");
+                            dogruMu = false;
+                            kontrol = false;
+                        }
+                    }
+                }
+            }
+            if (!isempty() && kontrol && acilan > kapanan)
+            {
+                MessageBox.Show("Kapama parantezinden eksik var.");
+                dogruMu = false;
+            }
+
+            if (dogruMu)
+            {
+                MessageBox.Show("Parantezlerin Doğru");
+            }
+        }
+
+        private void pop()
+        {
+            ilk = ilk.baglanti;
+        }
+
+        private char peek()
+        {
+            return ilk.deger;
+        }
+
+        private bool isempty()
+        {
+            if (ilk != null)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        private void push(char karakter)
+        {
+            dugum yeni = new dugum();
+            yeni.deger = karakter;
+            yeni.baglanti = ilk;
+            ilk = yeni;
+        }
+    }
+}
